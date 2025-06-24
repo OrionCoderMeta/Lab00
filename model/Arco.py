df48b88d6080427f9af40235dccab95dfd4da054
OrionCoderMeta <s307167@studenti.polito.it>
2025-06-24 19:08:50 +0200

    arco giusta

--- model/Arco.py
+++ model/Arco.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
-from model.pilot import Pilot
+from model.driver import Driver
 
 
 @dataclass
 class Arco:
-    n1 : Pilot
-    n2 : Pilot
+    n1 : Driver
+    n2 : Driver
     peso : float
 
     def __str__(self):
-        return f"{self.n1.id} -> {self.n2.id} | weight = {self.peso}"
+        return f"{self.n1.driverID} -> {self.n2.driverID} | weight = {self.peso}"
